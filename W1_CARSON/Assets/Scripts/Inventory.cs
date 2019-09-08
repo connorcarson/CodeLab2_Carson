cd057d3bc4475ed044594a5f10c72c0836d3a0fe
connorcarson <cc3237@nyu.edu>
2019-09-08 11:07:23 -0400

    Adding pixel art .meta files, added placeholder descriptors and variables to Item database in the Inspector.

---
+++ W1_CARSON/Assets/Scripts/Inventory.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+    public List<Item> inventory = new List<Item>();
+
+    private ItemDatabase _database;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        _database = GameObject.FindGameObjectWithTag("Item Database").GetComponent<ItemDatabase>();
+        
+        inventory.Add(_database.itemList[0]);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
