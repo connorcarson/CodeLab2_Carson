0bedf13735562629d7f49c7dd38633b75008d839
connorcarson <cc3237@nyu.edu>
2019-09-08 10:39:33 -0400

    Made Item class serializable and downloaded/added pixel pack of item icons.

--- W1_CARSON/Assets/Scripts/Item.cs
+++ W1_CARSON/Assets/Scripts/Item.cs
@@ -2,7 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Item : MonoBehaviour
+[System.Serializable]
+public class Item
 {
     public string itemName;
     public int itemID;
