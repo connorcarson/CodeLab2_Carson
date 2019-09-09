27e517a3116ba0b27b3b9f662c4a5125a072e080
connorcarson <cc3237@nyu.edu>
2019-09-09 00:35:44 -0400

    Fixed bug causing unity crashes, got GUI drawing to work successfully. Created a 5x4 slot grid of pixel art slots with corresponding inventory list, and assigned a few items to those slots with icon textures drawn on GUI.

--- W1_CARSON/Assets/Scripts/Inventory.cs
+++ W1_CARSON/Assets/Scripts/Inventory.cs
@@ -8,6 +8,7 @@ public class Inventory : MonoBehaviour
 {
     public int slotsX = 4;
     public int slotsY = 5;
+    public GUISkin slotSkin;
     private bool _showInventory;
     
     public List<Item> inventory = new List<Item>();
@@ -22,12 +23,15 @@ public class Inventory : MonoBehaviour
         for (int i = 0; i < (slotsX * slotsY); i++)
         {
             slots.Add(new Item());
+            inventory.Add(new Item());
         }
         
         _database = GameObject.FindGameObjectWithTag("ItemDatabase").GetComponent<ItemDatabase>();
-        inventory.Add(_database.itemList[0]);
-        inventory.Add(_database.itemList[3]);
-        inventory.Add(_database.itemList[4]);
+        
+        inventory[0] = _database.itemList[1];
+        inventory[1] = _database.itemList[2];
+        inventory[2] = _database.itemList[3];
+
     }
 
     // Update is called once per frame
@@ -36,29 +40,46 @@ public class Inventory : MonoBehaviour
         if (Input.GetButtonDown("Inventory"))
         {
             _showInventory = !_showInventory;
-            Debug.Log("Inventory!");
+            //Debug.Log("You've pressed the inventory button!");
         }    
     }
     
     private void OnGUI()
     {
+        GUI.skin = slotSkin;
+        
         if (_showInventory)
         {
             DrawInventory();
         }
-        for (int i = 0; i < inventory.Count; i++)
+        
+        /*for (int i = 0; i < inventory.Count; i++)
         {
             GUI.Label(new Rect(10, i * 20, 200, 50), inventory[i].itemName);
-        }
+        }*/
     }
 
     void DrawInventory()
     {
-        for (int x = 0; x < slotsX; x++)
+        int i = 0;
+        
+        for (int y = 0; y < slotsY; y++)
         {
-            for (int y = 0; y < slotsY; slotsY++)
+            for (int x = 0; x < slotsX; x++)
             {
-                GUI.Box(new Rect(x * 20, y * 20, 20, 20), y.ToString());
+                Rect slotRect = new Rect(x * 105, y * 105, 100, 100 );
+                //smaller icons with offset
+                //Rect itemRect = new Rect(x * 105 + 15, y * 105 + 15, 70, 70);
+                GUI.Box(slotRect, y.ToString(), slotSkin.GetStyle("Slot"));
+
+                slots[i] = inventory[i];
+
+                if (slots[i].itemName != null)
+                {
+                    GUI.DrawTexture(slotRect, slots[i].itemIcon);
+                }
+                
+                i++;
             }
         }
     }
