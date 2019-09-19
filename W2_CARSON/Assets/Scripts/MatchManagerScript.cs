3beaaf84a111eb61ac1b9412b377a1f3fa56632d
connorcarson <cc3237@nyu.edu>
2019-09-18 22:13:55 -0400

    Added GridHasVerticalMatch function. Bug: game currently breaks when there IS a vertical match.

--- W2_CARSON/Assets/Scripts/MatchManagerScript.cs
+++ W2_CARSON/Assets/Scripts/MatchManagerScript.cs
@@ -16,6 +16,11 @@ public class MatchManagerScript : MonoBehaviour {
 				if(x < _gameManager.gridWidth - 2){
 					match = match || GridHasHorizontalMatch(x, y);
 				}
+
+				if (y < _gameManager.gridHeight - 2)
+				{
+					match = match || GridHasVerticalMatch(x, y);
+				}
 			}
 		}
 		
@@ -26,6 +31,22 @@ public class MatchManagerScript : MonoBehaviour {
 		GameObject token1 = _gameManager.gridArray[x + 0, y];
 		GameObject token2 = _gameManager.gridArray[x + 1, y];
 		GameObject token3 = _gameManager.gridArray[x + 2, y];
+		
+		if(token1 != null && token2 != null && token3 != null){
+			SpriteRenderer sr1 = token1.GetComponent<SpriteRenderer>();
+			SpriteRenderer sr2 = token2.GetComponent<SpriteRenderer>();
+			SpriteRenderer sr3 = token3.GetComponent<SpriteRenderer>();
+			
+			return (sr1.sprite == sr2.sprite && sr2.sprite == sr3.sprite);
+		} else {
+			return false;
+		}
+	}
+	
+	public bool GridHasVerticalMatch(int x, int y){
+		GameObject token1 = _gameManager.gridArray[x, y + 0];
+		GameObject token2 = _gameManager.gridArray[x, y + 1];
+		GameObject token3 = _gameManager.gridArray[x, y + 2];
 		
 		if(token1 != null && token2 != null && token3 != null){
 			SpriteRenderer sr1 = token1.GetComponent<SpriteRenderer>();
