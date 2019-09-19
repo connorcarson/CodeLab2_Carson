4a82e589d0082f0b02387dd0b6d0ea1b614d9441
connorcarson <cc3237@nyu.edu>
2019-09-18 22:10:30 -0400

    committing updated unity library.

--- W2_CARSON/Assets/Scripts/MatchManagerScript.cs
+++ W2_CARSON/Assets/Scripts/MatchManagerScript.cs
@@ -1,78 +1,43 @@
 ﻿using UnityEngine;
 
-public class MatchManagerScript : MonoBehaviour
-{
-
+public class MatchManagerScript : MonoBehaviour {
+	
 	private GameManagerScript _gameManager;
 
-	public virtual void Start()
-	{
+	public virtual void Start () {
 		_gameManager = GetComponent<GameManagerScript>();
 	}
 
-	public virtual bool GridHasMatch()
-	{
+	public virtual bool GridHasMatch(){
 		bool match = false;
-
-		for (int x = 0; x < _gameManager.gridWidth; x++)
-		{
-			for (int y = 0; y < _gameManager.gridHeight; y++)
-			{
-				if (x < _gameManager.gridWidth - 2)
-				{
+		
+		for(int x = 0; x < _gameManager.gridWidth; x++){
+			for(int y = 0; y < _gameManager.gridHeight ; y++){
+				if(x < _gameManager.gridWidth - 2){
 					match = match || GridHasHorizontalMatch(x, y);
-				}
-
-				if (y < _gameManager.gridHeight - 2)
-				{
-					match = match || GridHasVerticalMatch(x, y);
 				}
 			}
 		}
-
+		
 		return match;
 	}
 
-	public bool GridHasHorizontalMatch(int x, int y)
-	{
+	public bool GridHasHorizontalMatch(int x, int y){
 		GameObject token1 = _gameManager.gridArray[x + 0, y];
 		GameObject token2 = _gameManager.gridArray[x + 1, y];
 		GameObject token3 = _gameManager.gridArray[x + 2, y];
-
-		if (token1 != null && token2 != null && token3 != null)
-		{
+		
+		if(token1 != null && token2 != null && token3 != null){
 			SpriteRenderer sr1 = token1.GetComponent<SpriteRenderer>();
 			SpriteRenderer sr2 = token2.GetComponent<SpriteRenderer>();
 			SpriteRenderer sr3 = token3.GetComponent<SpriteRenderer>();
-
+			
 			return (sr1.sprite == sr2.sprite && sr2.sprite == sr3.sprite);
-		}
-		else
-		{
+		} else {
 			return false;
 		}
 	}
 
-	public bool GridHasVerticalMatch(int x, int y)
-	{
-		GameObject token1 = _gameManager.gridArray[x, y + 0];
-		GameObject token2 = _gameManager.gridArray[x, y + 1];
-		GameObject token3 = _gameManager.gridArray[x, y + 2];
-
-		if (token1 != null && token2 != null && token3 != null)
-		{
-			SpriteRenderer sr1 = token1.GetComponent<SpriteRenderer>();
-			SpriteRenderer sr2 = token2.GetComponent<SpriteRenderer>();
-			SpriteRenderer sr3 = token3.GetComponent<SpriteRenderer>();
-
-			return (sr1.sprite == sr2.sprite && sr2.sprite == sr3.sprite);
-		}
-		else
-		{
-			return false;
-		}
-	}
-	
 	private int _GetHorizontalMatchLength(int x, int y){
 		int matchLength = 1;
 		
