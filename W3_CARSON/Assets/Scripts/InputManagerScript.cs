39a3223b2c9d8d44dad9687964824c613c58efc6
connorcarson <cc3237@nyu.edu>
2019-09-26 20:37:59 -0400

    changed comparisons in if statement in SelectToken function to int to get rid of "loss of precision" error.

--- W3_CARSON/Assets/Scripts/InputManagerScript.cs
+++ W3_CARSON/Assets/Scripts/InputManagerScript.cs
@@ -5,27 +5,30 @@ public class InputManagerScript : MonoBehaviour {
 	private GameManagerScript _gameManager;
 	private MoveTokensScript _moveManager;
 	private GameObject _selected = null;
+	private Camera _cam;
 
 	public virtual void Start () {
 		_moveManager = GetComponent<MoveTokensScript>();
 		_gameManager = GetComponent<GameManagerScript>();
+		_cam = Camera.main;
 	}
 
 	public virtual void SelectToken(){
 		if (Input.GetMouseButtonDown(0)){
-			Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+			Vector3 mousePos = _cam.ScreenToWorldPoint(Input.mousePosition);
 			
-			Collider2D collider = Physics2D.OverlapPoint(mousePos);
+			Collider2D tokenCollider = Physics2D.OverlapPoint(mousePos);
 
-			if(collider != null){
+			if(tokenCollider != null){
 				if(_selected == null){
-					_selected = collider.gameObject;
+					_selected = tokenCollider.gameObject;
 				} else {
 					Vector2 pos1 = _gameManager.GetPositionOfTokenInGrid(_selected);
-					Vector2 pos2 = _gameManager.GetPositionOfTokenInGrid(collider.gameObject);
+					Vector2 pos2 = _gameManager.GetPositionOfTokenInGrid(tokenCollider.gameObject);
 
-					if((Mathf.Abs(pos1.x - pos2.x) + Mathf.Abs(pos1.y - pos2.y)) == 3 && pos1.x != pos2.x && pos1.y != pos2.y){
-						_moveManager.SetupTokenExchange(_selected, pos1, collider.gameObject, pos2, true);
+					if((int)(Mathf.Abs(pos1.x - pos2.x) + Mathf.Abs(pos1.y - pos2.y)) == 3
+						&& (int)pos1.x != (int)pos2.x && (int)pos1.y != (int)pos2.y){
+						_moveManager.SetupTokenExchange(_selected, pos1, tokenCollider.gameObject, pos2, true);
 					}
 
 					_selected = null;
