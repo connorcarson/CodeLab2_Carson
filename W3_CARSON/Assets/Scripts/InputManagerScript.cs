dbfa8d129c9eb598f56967d30165d1887bfd2ae6
connorcarson <cc3237@nyu.edu>
2019-09-26 22:04:06 -0400

    Added IndicateTokenMatches method to InputManagerScript - it currently finds all possible moves based on the knight's move logic and highlights the swappable tokens by adjusting the Sprite Renderer's color and alpha.

--- W3_CARSON/Assets/Scripts/InputManagerScript.cs
+++ W3_CARSON/Assets/Scripts/InputManagerScript.cs
@@ -6,7 +6,13 @@ public class InputManagerScript : MonoBehaviour {
 	private MoveTokensScript _moveManager;
 	private GameObject _selected = null;
 	private Camera _cam;
+	private Vector2 pos1;
+	private Vector2 pos2;
 
+	public Color32 defaultColor;
+	public Color32 selectedColor;
+	public Color32 indicatedColor;
+	
 	public virtual void Start () {
 		_moveManager = GetComponent<MoveTokensScript>();
 		_gameManager = GetComponent<GameManagerScript>();
@@ -22,12 +28,16 @@ public class InputManagerScript : MonoBehaviour {
 			if(tokenCollider != null){
 				if(_selected == null){
 					_selected = tokenCollider.gameObject;
+					pos1 = _gameManager.GetPositionOfTokenInGrid(_selected);
+					IndicateTokenMatches(selectedColor, indicatedColor);
 				} else {
-					Vector2 pos1 = _gameManager.GetPositionOfTokenInGrid(_selected);
-					Vector2 pos2 = _gameManager.GetPositionOfTokenInGrid(tokenCollider.gameObject);
-
+					pos2 = _gameManager.GetPositionOfTokenInGrid(tokenCollider.gameObject);
+					IndicateTokenMatches(defaultColor, defaultColor);					
+					//if the absolute value of the tokens' positions is 3
 					if((int)(Mathf.Abs(pos1.x - pos2.x) + Mathf.Abs(pos1.y - pos2.y)) == 3
+						//and the tokens are not at the same position on either the x or y axis
 						&& (int)pos1.x != (int)pos2.x && (int)pos1.y != (int)pos2.y){
+						//then logically, it must be a "knight's move" and you should move those tokens!
 						_moveManager.SetupTokenExchange(_selected, pos1, tokenCollider.gameObject, pos2, true);
 					}
 
@@ -36,6 +46,22 @@ public class InputManagerScript : MonoBehaviour {
 			}
 		}
 
+	}
+
+	public void IndicateTokenMatches(Color32 token1Color, Color32 token2Color)
+	{ 
+		_selected.GetComponent<SpriteRenderer>().color = token1Color;
+		for (int x = 0; x < _gameManager.gridWidth; x++)
+		{
+			for (int y = 0; y < _gameManager.gridHeight; y++)
+			{
+				if ((int) (Mathf.Abs(pos1.x - x) + Mathf.Abs(pos1.y - y)) == 3
+				    && (int) pos1.x != x && (int) pos1.y != y)
+				{
+					_gameManager.gridArray[x, y].GetComponent<SpriteRenderer>().color = token2Color;
+				}
+			}
+		}
 	}
 
 	public int CommentFunc(int x, int y){
