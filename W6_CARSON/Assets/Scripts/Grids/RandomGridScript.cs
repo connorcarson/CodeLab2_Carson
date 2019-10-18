6b6839861a34f5923165dd5abe72b2e67d915f06
connorcarson <cc3237@nyu.edu>
2019-10-17 20:18:40 -0400

    Added commentary to RandomGridScript.

--- W6_CARSON/Assets/Scripts/Grids/RandomGridScript.cs
+++ W6_CARSON/Assets/Scripts/Grids/RandomGridScript.cs
@@ -15,33 +15,42 @@ public class RandomGridScript : GridScript
 
 	private void Awake()
 	{
+		// Make the grid be generated into gridString at random w/ the above percentages.
+		//for each row in our grid
 		for (int y = 0; y < gridHeight; y++)
 		{
+			//start with an empty string
 			string row = "";
+			//for each column in our grid
 			for (int X = 0; X < gridWidth; X++)
 			{
+				//if a random number between 0.0 and 1.0 is less than or equal to the chance of rocks
 				if (UnityEngine.Random.value <= rockPercentage)
 				{
+					//add the letter d to our empty string
 					row += "d";
 				}
-
+				//if a random number between 0.0 and 1.0 is less than or equal to the chance of forest
 				if (UnityEngine.Random.value <= forestPercentage)
 				{
+					//add the letter w to our empty string
 					row += "w";
 				}
-
+				//if a random number between 0.0 and 1.0 is less than or equal to the chance of water
 				if (UnityEngine.Random.value <= waterPercentage)
 				{
+					//add the letter r to our empty string
 					row += "r";
 				}
 				else
 				{
+					//add the meaningless '-' to our string
 					row += "-";
 				}
 			}
+			//add our randomly generated string to our list of strings
 			gridString.Add(row);
 		}
-		// Make the grid be generated into gridString at random w/ the above percentages.
 	}
 
 	protected override Material GetMaterial(int x, int y){
