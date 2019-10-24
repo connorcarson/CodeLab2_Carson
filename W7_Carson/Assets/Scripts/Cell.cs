8281ae6dba0c2ec668da676729d669204738cc6a
connorcarson <cc3237@nyu.edu>
2019-10-24 12:29:46 -0400

    WEEK 7: Added grid image, created a cell class, implementing an array of cells and created checks for horizontal and vertical wins. Still need a diagonal check.

---
+++ W7_Carson/Assets/Scripts/Cell.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+[System.Serializable] 
+public class Cell
+{
+    public bool isX;
+    public char gamepiece;
+
+    public Cell(bool isX)
+    {
+        this.isX = isX;
+        if (isX)
+        {
+            gamepiece = 'X';
+        }
+        else
+        {
+            gamepiece = 'O';
+        }
+    }
+
+    public Cell()
+    {
+        //empty constructor
+    }
+}
--- W7_Carson/Assets/Scripts/TicTacToeModel.cs
+++ W7_Carson/Assets/Scripts/TicTacToeModel.cs
@@ -4,15 +4,42 @@ using UnityEngine;
 
 public class TicTacToeModel : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private Cell[,] cells = new Cell[2,2];
+    private bool _isXTurn;
+    
     void Start()
     {
-        
+        _isXTurn = true;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void PlacePiece(int gridX, int gridY, bool isX)
     {
-        
+        cells[gridX, gridY] = new Cell(isX);
+    }
+
+    public bool VerticalWin()
+    {
+        for (int x = 0; x < 3; x++)
+        {
+            if (cells[x, 0].gamepiece == cells[x, 1].gamepiece || cells[x, 1].gamepiece == cells[x, 2].gamepiece)
+            {
+                Debug.Log(cells[x, 0].gamepiece + " wins!");
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public bool HorizontalWin()
+    {
+        for (int y = 0; y < 3; y++)
+        {
+            if (cells[0, y].gamepiece == cells[1, y].gamepiece || cells[1, y].gamepiece == cells[2, y].gamepiece)
+            {
+                Debug.Log(cells[0, y].gamepiece + " wins!");
+                return true;
+            }
+        }
+        return false;
     }
 }
