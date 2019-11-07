5ae32476124a93760fbfd0e3e9f18317c783e35e
connorcarson <cc3237@nyu.edu>
2019-11-07 12:53:07 -0500

    WEEK 9: attempting to create an AI that the player can play against. Created AI script that checks for valid moves. Not yet functional.

---
+++ W3_CARSON/Assets/Scripts/opponentController.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class opponentController : MonoBehaviour
+{
+    private GameManagerScript _gameManager;
+
+    private Vector2 pos1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _gameManager = GetComponent<GameManagerScript>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void LookForValidMove()
+    {
+        for (int x = 0; x < _gameManager.gridWidth; x++)
+        {
+            for (int y = 0; y < _gameManager.gridHeight; y++)
+            {
+                var toCheck = _gameManager.gridArray[x, y];
+                pos1 = _gameManager.GetPositionOfTokenInGrid(toCheck);
+
+                foreach (var move in ValidMoves(pos1))
+                {
+                    //if there's a match, add that move to a list of possible moves
+                    //rank according to the length of the match
+                    //make (one of) the move(s) with the longest possible match
+                }
+            }
+        }
+    }
+
+    Vector2[] ValidMoves(Vector2 pos1)
+    {
+        Vector2[] validMoves = new[]
+        {
+            new Vector2(pos1.x + 2, pos1.y + 1), 
+            new Vector2(pos1.x + 2, pos1.y - 1),
+            new Vector2(pos1.x - 2, pos1.y + 1),
+            new Vector2(pos1.x - 2, pos1.y - 1),
+            new Vector2(pos1.x + 1, pos1.y + 2),
+            new Vector2(pos1.x - 1, pos1.y + 2), 
+            new Vector2(pos1.x + 1, pos1.y - 2),
+            new Vector2(pos1.x - 1, pos1.y - 2),
+        };
+        return validMoves;
+    }
+}
