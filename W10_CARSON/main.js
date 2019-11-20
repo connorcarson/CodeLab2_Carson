1ae294cbca28e2db1231eaaece01dab2de79268b
connorcarson <cc3237@nyu.edu>
2019-11-20 17:28:55 -0500

    WEEK 11: Not sure if phaser is taking into account the .js file I added to my document root, but I'm now just implementing that code in my .html file. Checking for key alphanumeric key presses and logging them to the console/pushing them into array.

--- W10_CARSON/main.js
+++ W10_CARSON/main.js
@@ -1,5 +1,13 @@
 document.addEventListener('DOMContentLoaded', () => {
     'use strict';
 
-    console.log('content loaded');
+    document.addEventListener('keydown', event => {
+        const charList = 'abcdefghijklmnopqrstuvwxyz0123456789';
+        const key = event.key.toLowerCase();
+
+        // we are only interested in alphanumeric keys
+        if (charList.indexOf(key) === -1) return;
+
+        console.log(key);
+    });
 });
